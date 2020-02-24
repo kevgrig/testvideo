4eb6f86dd06658912bd0c5ec47070cc743d27860
Kevin G <kevin@myplaceonline.com>
2020-02-23 19:40:07 -0800

    Add carousel

--- App.js
+++ App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import { Dimensions, Text, View } from 'react-native';
 import { Video } from 'expo-av';
 import VideoPlayer from 'expo-video-player';
+import Carousel from 'react-native-snap-carousel';
 
 export default function App() {
   const { width: viewportWidth, height: viewportHeight } = Dimensions.get("window");
   return (
-    <View style={{flex: 1, borderWidth: 1, borderColor: "#000" }}>
-      <VideoPlayer
-        videoProps={{
-          source: {
-            uri: 'http://myplaceonline.com/frontend/BigBuckBunnyCut.mp4',
-          },
-          resizeMode: Video.RESIZE_MODE_CONTAIN,
-          shouldPlay: false,
-          isLooping: false,
-          width: "100%",
-          height: "100%",
+    <View style={{ flex: 1 }}>
+      <Carousel
+        data={[1, 2]}
+        sliderWidth={viewportWidth}
+        itemWidth={viewportWidth*.8}
+        renderItem={(item, index) => {
+          return (
+            <View style={{ flex: 1, borderWidth: 1, borderColor: "red" }}>
+              <VideoPlayer
+                videoProps={{
+                  source: {
+                    uri: 'http://myplaceonline.com/frontend/BigBuckBunnyCut.mp4',
+                  },
+                  resizeMode: Video.RESIZE_MODE_CONTAIN,
+                  shouldPlay: false,
+                  isLooping: false,
+                  width: (viewportWidth*.8)-5,
+                  height: viewportHeight,
+                }}
+                inFullscreen={false}
+              />
+            </View>
+          );
         }}
-        inFullscreen={false}
       />
     </View>
   );
