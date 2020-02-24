1327677d88ef5cf055e10fa29d97277815c86597
Kevin G <kevin@myplaceonline.com>
2020-02-23 18:32:55 -0800

    Add VideoPlayer example

--- App.js
+++ App.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import { Video } from 'expo-av';
+import VideoPlayer from 'expo-video-player';
 
 export default function App() {
+  const { width: viewportWidth, height: viewportHeight } = Dimensions.get("window");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{flex: 1, borderWidth: 1, borderColor: "#000" }}>
+      <VideoPlayer
+        videoProps={{
+          source: {
+            uri: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+          },
+          resizeMode: Video.RESIZE_MODE_CONTAIN,
+          shouldPlay: false,
+          isLooping: false,
+          width: "100%",
+          height: "100%",
+        }}
+        inFullscreen={false}
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
